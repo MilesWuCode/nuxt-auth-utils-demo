e405e4dd02ad1d42ace344e79f248651d43049bf
Miles Wu <slion0725@gmail.com>
2025-03-18 16:53:46 +0800

    upd

--- utils/demo.ts
+++ utils/demo.ts
@@ -1,9 +1,15 @@
 export function createAccessTokenExpiredAt() {
   // 30秒
-  return Date.now() + 30 * 1000;
+  // return Date.now() + 30 * 1000;
+
+  // 1小時
+  return Date.now() + 3600 * 1000;
 }
 
 export function createRefreshTokenExpiredAt() {
   // 60秒
-  return Date.now() + 60 * 1000;
+  // return Date.now() + 60 * 1000;
+
+  // 2小時
+  return Date.now() + 2 * 3600 * 1000;
 }
