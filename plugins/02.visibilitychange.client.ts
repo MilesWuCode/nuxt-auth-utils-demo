a4cca058b9f26bf4573df27cc52fad7a98ee3b50
Miles Wu <slion0725@gmail.com>
2025-04-22 17:45:44 +0800

    upd

--- plugins/02.visibilitychange.client.ts
+++ plugins/02.visibilitychange.client.ts
@@ -4,13 +4,26 @@ export default defineNuxtPlugin(() => {
   const handleVisibilityChange = async () => {
     isVisible.value = document.visibilityState === "visible";
 
-    const { fetch } = useUserSession();
+    const { loggedIn, fetch, clear } = useUserSession();
 
     if (isVisible.value) {
       // 返回瀏覽器
 
-      // 無關登入先更新session
-      await fetch();
+      if (loggedIn.value) {
+        // 不同瀏覽器的同步
+        // 順便檢查authorization是否合法
+        await $fetch("/api/me")
+          .then(async () => {
+            await fetch();
+          })
+          .catch(async () => {
+            // 失敗責登出
+            await clear();
+          });
+      } else {
+        // 未登入時同步瀏覽器分頁是否有登入
+        await fetch();
+      }
 
       // 若是登出則觸發01.auth-redirect.client
     } else {
