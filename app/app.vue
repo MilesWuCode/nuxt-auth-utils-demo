cb06ef18c47fb7a7544b3b7a1ea78db759063d74
miles <slion0725@gmail.com>
2026-05-28 17:07:44 +0800

    upd

--- app/app.vue
+++ app/app.vue
@@ -17,6 +17,21 @@ watch(
     open.value = false
   },
 )
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Site Title` : 'Site Title'
+  },
+})
+
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
