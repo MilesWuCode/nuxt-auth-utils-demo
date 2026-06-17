d19b2625cc3c084297f4fd65d7396f489718d2b1
miles <slion0725@gmail.com>
2026-06-17 13:37:38 +0800

    upd

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,30 +2,30 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
   rules: {
-    'no-console': 'off',
-    'vue/html-self-closing': 'off',
-    'vue/no-v-html': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/padding-line-between-blocks': ['error', 'always'],
+    'no-console': 'off', // 允許 console
+    'vue/html-self-closing': 'off', // 允許非自閉合標籤
+    'vue/no-v-html': 'off', // 允許 v-html
+    'vue/multi-word-component-names': 'off', // 允許單字元件名稱
+    'vue/padding-line-between-blocks': ['error', 'always'], // script template style 之間必須空行
     'vue/block-lang': [
       'error',
       {
         script: {
-          lang: 'ts',
+          lang: 'ts', // script 必須使用 TypeScript
         },
       },
     ],
     'vue/block-order': [
       'error',
       {
-        order: ['script', 'template', 'style'],
+        order: ['script', 'template', 'style'], // 區塊順序：script > template > style
       },
     ],
     'vue/define-macros-order': [
       'warn',
       {
-        order: ['defineOptions', 'defineProps', 'defineModel', 'defineEmits'],
-        defineExposeLast: true,
+        order: ['defineOptions', 'defineProps', 'defineModel', 'defineEmits'], // macro 宣告順序
+        defineExposeLast: true, // defineExpose 放最後
       },
     ],
   },
