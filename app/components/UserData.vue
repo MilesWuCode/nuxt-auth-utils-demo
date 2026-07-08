40448f441e0e253c4661b212316d57398c6182c9
miles <slion0725@gmail.com>
2026-07-08 16:35:27 +0800

    upd

--- app/components/UserData.vue
+++ app/components/UserData.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 const { loggedIn } = useUserSession()
 
-const { data, refresh } = await useApi('/api/me')
+// 沒登入就不要打/api/me,不然401會被useAuthFetchOptions的onResponseError攔截導去/login
+const { data, refresh } = await useApi('/api/me', {
+  immediate: loggedIn.value,
+})
 </script>
 
 <template>
