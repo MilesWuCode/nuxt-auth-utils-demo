ae57f3ac4582ecac7aa49af712b011f9e69bf9f4
miles <slion0725@gmail.com>
2026-07-21 15:46:37 +0800

    upd

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
-const { data } = await useAsyncData('blogs', () =>
-  queryCollection('blog').all(),
+const route = useRoute()
+const limit = 1
+const currentPage = computed(() => parseInt(route.query.page as string) || 1)
+
+const { data: total } = await useAsyncData('blog-count', () => {
+  return queryCollection('blog').count()
+})
+
+const { data: posts } = await useAsyncData(
+  `blogs-${currentPage.value}`,
+  () => {
+    return queryCollection('blog')
+      .order('date', 'DESC') // 通常會按日期排序
+      .limit(limit)
+      .skip((currentPage.value - 1) * limit)
+      .all()
+  },
+  {
+    watch: [currentPage],
+  },
 )
+
+function to(page: number) {
+  return {
+    query: {
+      page,
+    },
+  }
+}
 </script>
 
 <template>
@@ -11,13 +37,20 @@ const { data } = await useAsyncData('blogs', () =>
       <UContainer>
         <UBlogPosts>
           <UBlogPost
-            v-for="(post, index) in data"
+            v-for="(post, index) in posts"
             :key="index"
             v-bind="post"
             :to="post.path"
           />
         </UBlogPosts>
       </UContainer>
+      <UPagination
+        v-model:page="currentPage"
+        :total="total || 0"
+        :items-per-page="limit"
+        :to="to"
+        class="flex justify-center"
+      />
     </UPageBody>
   </UPage>
 </template>
--- content/blog/car2.md
+++ content/blog/car2.md
@@ -1,7 +1,7 @@
 ---
 title: 'H2O-My-God! The Ultimate Guide to Not Turning Into a Human Raisin'
 description: Learn why drinking water is important, because it actually is. Discover tips and tricks to stay hydrated and never miss a glass of water.
-date: 2025-08-29
+date: 2026-03-02
 tags: [tutorial, health]
 image: ''
 ---
