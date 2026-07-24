f5d0416942154dd52807814511b7b037ef503a39
miles <slion0725@gmail.com>
2026-07-24 15:42:06 +0800

    upd

--- app/plugins/05.auth-cookie.client.ts
+++ app/plugins/05.auth-cookie.client.ts
@@ -1,13 +1,19 @@
 // 第三方登入狀態改變時
 // 由 server 端寫入 authSuccess cookie
-// 其他分頁監看到這個 cookie 變化就強制 reloadNuxtApp
+// 分頁監看到這個 cookie 變化就強制 reloadNuxtApp
 
 export default defineNuxtPlugin(() => {
   const authSuccessCookie = useCookie('authSuccess')
+  const { loggedIn } = useUserSession()
 
   watch(authSuccessCookie, (value) => {
     if (!value) return
 
-    reloadNuxtApp({ force: true })
+    // 已經登入的頁面不需要reload，若沒有等待1秒，原分頁會換到首頁
+    setTimeout(() => {
+      if (loggedIn.value) return
+
+      reloadNuxtApp({ force: true })
+    }, 1000)
   })
 })
